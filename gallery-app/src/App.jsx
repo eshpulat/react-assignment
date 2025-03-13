14f310cf0f20a57b29a5ba78afb605e73ecbe763
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-03-13 18:16:48 -0400

    finish galary app

--- gallery-app/src/App.jsx
+++ gallery-app/src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import img1 from "./assets/img-1.jpg";
+import img2 from "./assets/img-2.jpg";
+import img3 from "./assets/img-3.jpg";
+import img4 from "./assets/img-4.jpg";
+import img5 from "./assets/img-5.jpg";
+import img6 from "./assets/img-6.jpg";
+import img7 from "./assets/img-7.jpg";
+import img8 from "./assets/img-8.jpg";
+import img9 from "./assets/img-9.jpg";
+import img10 from "./assets/img-10.jpg";
+import img11 from "./assets/img-11.jpg";
+import img12 from "./assets/img-12.jpg";
+import img13 from "./assets/img-13.jpg";
+import img14 from "./assets/img-14.jpg";
+import img15 from "./assets/img-15.jpg";
+import img16 from "./assets/img-16.jpg";
+import img17 from "./assets/img-17.jpg";
+import { useState } from "react";
+
+const imgBox = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [openImg, setOpenImg] = useState(false);
+    const [isModelOpen, setIsModelOpen] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // function handleOpen(img) {
+    //     setIsModelOpen(img);
+    //     setOpenImg(true);
+    // }
+
+    const openModel = (img) => {
+        setIsModelOpen(img);
+        setOpenImg(true);
+    };
+
+    const closeModel = () => {
+        setIsModelOpen(null);
+        setOpenImg(false);
+    };
+
+    return (
+        <div>
+            <h1>Gallery App</h1>
+            <div className="img-item">
+                {imgBox.map((img, index) => (
+                    <img
+                        key={index}
+                        src={img}
+                        alt={`Gallery images ${index + 1}`}
+                        onClick={() => openModel(img)}
+                    />
+                ))}
+            </div>
+            {openImg && (
+                <div className="open-img">
+                    <div className="img-box">
+                        <img src={isModelOpen} />
+                        <button className="close-btn" onClick={closeModel}>
+                            &times;
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default App
+export default App;
